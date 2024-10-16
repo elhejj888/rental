1db7ebb1ff623a93871fc782a7aae775f6a63ddf
elhejj888 <elhejjiouiyoussef@gmail.com>
2024-10-16 20:07:01 +0100

    test

--- app/api/cars/route.js
+++ app/api/cars/route.js
@@ -3,12 +3,6 @@ import prisma from '../../lib/prisma';
 
 export async function POST(req) {
 
-  const session = await getSession({ req });
-
-  if (!session) {
-    return res.status(401).json({ message: 'Unauthorized' });
-  }
-
   const { name, image, modele, marque, year, price } = await req.json();
 
   const yearInt = parseInt(year);
